4baebe331aaa79956b87981eee45819aa36e9b1d
rahul kumar <ranjitkumar85453@gmail.com>
2025-06-01 11:09:01 +0530

    initial prject structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style = {{padding: '2 rem'}}>
+      <h1> Welcome to the FinGenie</h1>
+      <p> Your AI-powered Business finance copilot</p>
+
+    </div>
+  );
+}
+
+export default App;
